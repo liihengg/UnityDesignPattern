4ee2d6d1a1f961a139db02ff2af140c4c4b92619
supercollli <819169384@qq.com>
2020-12-16 16:24:19 +0800

    Signed-off-by: supercollli 测试远程推送

---
+++ Assets/Patterns/Factory/Li_Factory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Li_Factory
+{
+    public static Api CreateApi(int condition)
+    {
+        Api api = null;
+        if(condition == 1)
+        {
+            api = new ImplA();
+        }
+        else
+        {
+            api = new ImplB();
+        }
+        return api;
+    }
+
+}
+
+
+public class ImplA : Api
+{
+    public void Operation(string s)
+    {
+        Debug.Log("ImplA" + s);
+    }
+}
+
+public class ImplB : Api
+{
+    public void Operation(string s)
+    {
+        Debug.Log("ImplB" + s);
+    }
+}
+
+public interface Api
+{
+    void Operation(string s);
+}
+
---
+++ Assets/Patterns/Factory/Li_FactoryClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Li_FactoryClient : MonoBehaviour
+{
+
+    void Start()
+    {
+        Api api = Li_Factory.CreateApi(1);
+        api.Operation("使用工厂");
+    }
+
+
+}
